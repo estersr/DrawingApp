406e95aa0ebff89bee906c85d238d3510aa04ff9
estersr <87035589+estersr@users.noreply.github.com>
2022-06-14 17:38:56 -0300

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import Foundation
+
+struct Constants {
+    struct Icons {
+        static let plusCircle = "plus.circle"
+        static let line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+        static let circle = "circle"
+        static let circleInsertFilled = "circle.insert.filled"
+        static let exclamationMarkCircle = "exclamationark.circle"
+        static let recordCircleFill = "record.circle.fill"
+        static let trayCircleFill = "tray.circle.fill"
+        static let circleFill = "circle.fill"
+        
+        
+    }
+}
